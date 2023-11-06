9d69e9e2d0a1e2d46e71341dfc4804b31e1a4e52
Croesus0303 <45857730+Croesus0303@users.noreply.github.com>
2023-11-06 21:08:12 +0300

    Delete TrendsGit directory

--- TrendsGit/src/main/java/com/bot/trends/utility/ServiceHelper.java
+++
@@ -1,9 +0,0 @@
-package com.bot.trends.utility;
-
-import com.bot.trends.model.response.NewsResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mashape.unirest.http.HttpResponse;
-
-public class ServiceHelper {
-
-}
